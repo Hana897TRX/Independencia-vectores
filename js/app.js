b0bf35fa9c8540bf25f8b77073f77ec815ffb2ab
Hana897TRX <57414872+Hana897TRX@users.noreply.github.com>
2020-10-12 18:12:07 -0500

    First Dynamic TextBox (1.0)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+let matrix = [];
+
 function getInformation(){
-    var vertNumber = document.getElementById("inputText").value;
-    var configWait = document.getElementById("inputVectors");
+    let vertNumber = document.getElementById("inputText").value;
+    let configWait = document.getElementById("inputVectors");
 
     //Verification variable
     if (isNaN(vertNumber) || vertNumber == ""){
@@ -10,8 +12,27 @@ function getInformation(){
     else{
         //Show new section and hide others
         configWait.style.display = "none";
-        var temp = document.getElementsByTagName("template")[0];
-        var clon = temp.content.cloneNode(true);
+        let temp = document.getElementsByTagName("template")[0];
+        let clon = temp.content.cloneNode(true);
         document.body.appendChild(clon);
+        GenerateInputText(3, 3);
+    }
+}
+
+function GenerateInputText(col, rows){
+    let addSpace = '<br>';
+    let addRow = '<div id="row" class="row"></div>';
+    let addCol = '<div class="col"></div>';
+    let addInput = '<input id="inputText" type="text" class="form-control">';
+    let position = document.getElementById("vectorsContent");
+    for(var y = 0; y < rows; y++){
+        position.innerHTML += addRow;
+        addInput.replace("row", "row" + x);
+        //let positionCol = document.getElementById("row" + x);
+        for(var x = 0; x < col; x++){
+            position.innerHTML += addSpace;
+            position.innerHTML += addCol;
+            position.innerHTML += addInput;
+        }
     }
 }
