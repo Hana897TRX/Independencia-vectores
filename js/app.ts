6f44cb6fe2501903bd499fb7d5e77c76206fbf94
Hana897TRX <57414872+Hana897TRX@users.noreply.github.com>
2020-10-13 16:23:55 -0500

    broken

--- js/app.ts
+++ js/app.ts
@@ -1,41 +1,66 @@
 let number = 0;
 let active = false;
+let inputVectors;
+let contVectors;
+
+window.onload = chargeListeners;
+
+function chargeListeners(){
+    var btnAccept = document.getElementById("btnAccept");
+    btnAccept.addEventListener("click", getInformation);
+}
 
 function GetOptionRX(){
-    if(document.getElementById("r2").checked)
+    let r2 = (<HTMLInputElement>document.getElementById("r2")).checked;
+    let r3 = (<HTMLInputElement>document.getElementById("r3")).checked;
+    let r4 = (<HTMLInputElement>document.getElementById("r4")).checked;
+
+    if(r2)
         return 2;
-    else if(document.getElementById("r3").checked)
+    else if(r3)
         return 3;
-    else if(document.getElementById("r4").checked)
+    else if(r4)
         return 4;
     else
         return -3;
 }
 
 function removeTextBox(){
+    var vectorsContent = document.getElementById("vectorsContent");
+    var btnComprobate = document.getElementById("btnComprobate");
+    var controlVectors = document.getElementById("controlVectors");
+
     if(active){
         for(var i = 0; i < number; i++){
             var element = document.getElementById("textBox" + i.toString());
             element.parentNode.removeChild(element);
         }
+
         number = 0;
+        
+        //Remove previous
         vectorsContent.parentNode.removeChild(vectorsContent);
         btnComprobate.parentNode.removeChild(btnComprobate);
+
+        //Add new Parts
         controlVectors.innerHTML += '<div id="vectorsContent" class="container-fluid"></div>';
         controlVectors.innerHTML += '<button id="btnComprobate"type="button" class="btn btn-primary btn-lg btn-block">Comprobar linealidad.</button>';
+        
+        //Hide Content
         inputVectors.style.display = 'contents';
         contVectors.style.display = 'none';
     }
 }
 
 function getInformation(){
-    let vertNumber = document.getElementById("inputText").value;
-    let configWait = document.getElementById("inputVectors");
+    var vertNumber = (document.getElementById("inputText") as HTMLInputElement).value;
+    inputVectors = document.querySelector("inputVectors");
+    contVectors = document.querySelector("contVectors");
     let dimension = GetOptionRX();
     console.log(dimension);
 
     //Verification part
-    if (isNaN(vertNumber) || vertNumber == ""){
+    if (vertNumber == "" || vertNumber.match(/[A-Z][a-z]/g)){
         alert("Input must be a number.");
         return -1;
     }
@@ -46,17 +71,17 @@ function getInformation(){
     else{
         //Show new section and hide others
         if(!active){
-            configWait.style.display = "none";
+            inputVectors.style.display = "none";
             let temp = document.getElementsByTagName("template")[0];
             let clon = temp.content.cloneNode(true);
             document.body.appendChild(clon);
             active = true;
         }
-        GenerateInputText(dimension, vertNumber);
+        GenerateInputText(dimension, parseInt(vertNumber));
     }
 }
 
-function GenerateInputText(col, rows){
+function GenerateInputText(col : number, rows : number){
     inputVectors.style.display = 'none';
     contVectors.style.display = 'contents';
     let addSpace = '<br>';
