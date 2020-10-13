0d2f92ff9c0d8602caf20d09355416711b6702f9
Hana897TRX <57414872+Hana897TRX@users.noreply.github.com>
2020-10-12 21:21:00 -0500

    Dynamic TextBox Working

--- js/app.js
+++ js/app.js
@@ -15,28 +15,29 @@ function getInformation(){
         let temp = document.getElementsByTagName("template")[0];
         let clon = temp.content.cloneNode(true);
         document.body.appendChild(clon);
-        GenerateInputText(2, 2);
+        GenerateInputText(3, 3);
     }
 }
 
 function GenerateInputText(col, rows){
+    let number = 0;
     let addSpace = '<br>';
-    let addRow = '<div id="row" class="row"></div>';
-    let addCol = '<div class="col"></div>';
-    let addInput = '<input id="inputText" type="text" class="form-control">';
+    let addRow1 = '<div id="';
+    let addRow2 = '" class="row"></div>'
+    let addCol = '<div class="col"><input id="';
+    let addInput2 = '" type="text" class="form-control"></div>';
     let position = document.getElementById("vectorsContent");
     for(var y = 0; y < rows; y++){
-        let temprow = addRow;
-        temprow.replace("row", "row" + y.toString());
+        let temprow = addRow1 + "row" + y.toString() + addRow2;
         position.innerHTML += temprow;
-        
-        //let positionCol = document.getElementById("row" + x);
+        let supPosition = document.getElementById("row" + y.toString())
         for(var x = 0; x < col; x++){
-            let tempInput = addInput;
-            position.innerHTML += addSpace;
-            tempInput.replace("inputText", "inputText" + x.toString());
-            position.innerHTML += addCol;
-            position.innerHTML += tempInput;
+            let tempInput = addCol + "tempText" + number.toString() + addInput2;
+            supPosition.innerHTML += addSpace;
+            supPosition.innerHTML += tempInput;
+            supPosition.innerHTML += addSpace;
+            supPosition.innerHTML += addSpace;
+            number++;
         }
     }
 }
