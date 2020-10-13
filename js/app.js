d12641100b325fc6cf393bcce3eaacbe1dadef00
Hana897TRX <57414872+Hana897TRX@users.noreply.github.com>
2020-10-12 22:34:22 -0500

    radio buttons function added and verification improvement

--- js/app.js
+++ js/app.js
@@ -1,26 +1,53 @@
-let matrix = [];
+let matrix;
+let number = 0;
+let active = false;
+
+function GetOptionRX(){
+    if(document.getElementById("r2").checked)
+        return 2;
+    else if(document.getElementById("r3").checked)
+        return 3;
+    else if(document.getElementById("r4").checked)
+        return 4;
+    else
+        return -3;
+}
 
 function getInformation(){
     let vertNumber = document.getElementById("inputText").value;
     let configWait = document.getElementById("inputVectors");
-
+    let dimension = GetOptionRX();
+    console.log(dimension);
     //Verification variable
     if (isNaN(vertNumber) || vertNumber == ""){
         alert("Input must be a number.");
         return -1;
     }
+    else if(dimension == -3){
+        alert("You must select Vector dimension.");
+        return -2;
+    }
     else{
         //Show new section and hide others
-        configWait.style.display = "none";
-        let temp = document.getElementsByTagName("template")[0];
-        let clon = temp.content.cloneNode(true);
-        document.body.appendChild(clon);
-        GenerateInputText(3, 3);
+        if(!active){
+            configWait.style.display = "none";
+            let temp = document.getElementsByTagName("template")[0];
+            let clon = temp.content.cloneNode(true);
+            document.body.appendChild(clon);
+            active = true;
+        }
+        GenerateInputText(dimension, vertNumber);
     }
 }
 
+function Create2DArray(x, y){
+    matrix = new Array(x);
+    for(var i = 0; i < matrix.length; i++)
+        matrix[i] = new Array(x);
+}
+
 function GenerateInputText(col, rows){
-    let number = 0;
+    Create2DArray(col, rows);
     let addSpace = '<br>';
     let addRow1 = '<div id="';
     let addRow2 = '" class="row"></div>'
@@ -37,6 +64,8 @@ function GenerateInputText(col, rows){
             supPosition.innerHTML += tempInput;
             supPosition.innerHTML += addSpace;
             supPosition.innerHTML += addSpace;
+            matrix[y][x] = "tempText" + number.toString();
+                console.debug(matrix[y][x]);
             number++;
         }
     }
